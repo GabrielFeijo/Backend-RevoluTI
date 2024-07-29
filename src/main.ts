628d0467335439f5fe3a14d0ad34f7790d08b9bb
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-07-29 16:16:58 -0300

    chore: add Swagger configuration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  app.enableCors({ origin: '*' });
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('/api/v1');
+
+  const port = process.env.PORT || 3000;
+
+  const options = new DocumentBuilder()
+    .setTitle('Desafio RevolutiTI - Gabriel Feijó')
+    .setDescription(
+      'API para gerenciamento de endereços, permitindo a criação, atualização, exclusão e consulta de endereços.',
+    )
+    .setVersion('1.0')
+    .addServer(`http://localhost:${port}/`, 'Local environment')
+    .addServer('https://production.revoluti.com/', 'Production')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/v1/documentation', app, document);
+
+  await app.listen(port);
 }
 bootstrap();
