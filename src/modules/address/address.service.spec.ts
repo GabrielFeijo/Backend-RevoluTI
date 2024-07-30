d0fd6e4a9534dd7d8d0c7de52af137944cfda4d5
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-07-30 18:29:28 -0300

    test: add new tests for the project

--- src/modules/address/address.service.spec.ts
+++ src/modules/address/address.service.spec.ts
@@ -1,139 +1,96 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from 'src/database/prisma.service';
 import { AddressService } from './address.service';
-
 import { faker } from '@faker-js/faker';
-import { PrismaService } from '../../database/prisma.service';
 import { AddressRepository } from './address.repository';
-import { INestApplication } from '@nestjs/common';
 
-const fakeAddresses = [
-  {
-    id: faker.string.uuid(),
-    sessionId: faker.string.uuid(),
-    street: faker.location.street(),
-    postalCode: faker.location.zipCode(),
-    city: faker.location.city(),
-    state: faker.location.state(),
-    country: faker.location.country(),
-  },
-  {
-    id: faker.string.uuid(),
-    sessionId: faker.string.uuid(),
-    street: faker.location.street(),
-    postalCode: faker.location.zipCode(),
-    city: faker.location.city(),
-    state: faker.location.state(),
-    country: faker.location.country(),
-  },
-  {
-    id: faker.string.uuid(),
-    sessionId: faker.string.uuid(),
-    street: faker.location.street(),
-    postalCode: faker.location.zipCode(),
-    city: faker.location.city(),
-    state: faker.location.state(),
-    country: faker.location.country(),
-  },
-];
-
-const prismaMock = {
-  address: {
-    create: jest.fn().mockReturnValue(fakeAddresses[0]),
-    findMany: jest.fn().mockResolvedValue(fakeAddresses),
-    findUnique: jest.fn().mockResolvedValue(fakeAddresses[0]),
-    update: jest.fn().mockResolvedValue(fakeAddresses[0]),
-    delete: jest.fn(),
-  },
+const address = {
+  id: '4cd59115-6981-4610-8cba-e895a2357916',
+  sessionId: faker.string.uuid(),
+  street: faker.location.street(),
+  postalCode: faker.location.zipCode(),
+  city: faker.location.city(),
+  state: faker.location.state(),
+  country: faker.location.country(),
 };
 
-describe('AddressService', () => {
-  let app: INestApplication;
-  let service: AddressService;
-  let prisma: PrismaService;
+const updatedAddress = {
+  ...address,
+  street: faker.location.street(),
+};
+
+describe('AddressService - Main Flow', () => {
+  let sut: AddressService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        AddressService,
-        AddressRepository,
-        { provide: PrismaService, useValue: prismaMock },
-      ],
+      providers: [AddressService, AddressRepository, PrismaService],
     }).compile();
 
-    app = module.createNestApplication();
-    await app.init();
-
-    service = module.get<AddressService>(AddressService);
-    prisma = module.get<PrismaService>(PrismaService);
+    sut = module.get<AddressService>(AddressService);
   });
 
-  afterEach(() => {
-    jest.clearAllMocks();
+  it('should be defined', () => {
+    expect(sut).toBeDefined();
   });
 
-  describe('findAll', () => {
-    it(`should return an array of addresses`, async () => {
-      const response = await service.getAllAddresses();
+  it('should create a new address and return it', async () => {
+    const result = await sut.createAddress(address);
 
-      expect(response).toEqual(fakeAddresses);
-    });
+    expect(result).toBeDefined();
+    expect(result).toHaveProperty('id');
+    expect(result).toHaveProperty('createdAt');
+    expect(result).toHaveProperty('updatedAt');
+    expect(result).toMatchObject(address);
   });
 
-  describe('findOne', () => {
-    it(`should return a single address`, async () => {
-      const response = await service.getAddressById(fakeAddresses[0].id);
+  it('should find an address by id', async () => {
+    const result = await sut.getAddressById(address.id);
 
-      expect(response).toEqual(fakeAddresses[0]);
-      expect(prisma.address.findUnique).toHaveBeenCalledTimes(1);
-      expect(prisma.address.findUnique).toHaveBeenCalledWith({
-        where: { id: fakeAddresses[0].id },
-      });
-    });
+    expect(result).toBeDefined();
+    expect(result).toMatchObject(address);
   });
 
-  describe('create', () => {
-    it(`should create a new address`, async () => {
-      const response = await service.createAddress(fakeAddresses[0]);
-
-      expect(response).toBe(fakeAddresses[0]);
-      expect(prisma.address.create).toHaveBeenCalledTimes(1);
-      expect(prisma.address.create).toHaveBeenCalledWith({
-        data: fakeAddresses[0],
-      });
-    });
+  it('should return a 404 error when address is not found', async () => {
+    const nonExistentId = 'non-existent-id';
+    try {
+      await sut.getAddressById(nonExistentId);
+    } catch (error) {
+      expect(error.status).toBe(404);
+      expect(error.message).toBe('Address not found');
+    }
   });
 
-  describe('updateOne', () => {
-    const updateAddressDto = {
-      street: faker.location.street(),
-      postalCode: faker.location.zipCode(),
-      city: faker.location.city(),
-      state: faker.location.state(),
-      country: faker.location.country(),
-    };
+  it('should update an existing address and return it', async () => {
+    const result = await sut.updateAddress(address.id, updatedAddress);
 
-    it(`should update a single address`, async () => {
-      const response = await service.updateAddress(
-        fakeAddresses[0].id,
-        updateAddressDto,
-      );
-
-      expect(response).toEqual(fakeAddresses[0]);
-      expect(prisma.address.update).toHaveBeenCalledTimes(1);
-      expect(prisma.address.update).toHaveBeenCalledWith({
-        where: { id: fakeAddresses[0].id },
-        data: updateAddressDto,
-      });
-    });
+    expect(result).toBeDefined();
+    expect(result).toMatchObject(updatedAddress);
   });
 
-  describe('deleteOne', () => {
-    it(`should delete address by id`, async () => {
-      expect(await service.deleteAddress(fakeAddresses[0].id)).toBeUndefined();
-      expect(prisma.address.delete).toHaveBeenCalledTimes(1);
-      expect(prisma.address.delete).toHaveBeenCalledWith({
-        where: { id: fakeAddresses[0].id },
-      });
-    });
+  it('should return a 404 error when updating an address with invalid id', async () => {
+    const nonExistentId = 'non-existent-id';
+    try {
+      await sut.updateAddress(nonExistentId, updatedAddress);
+    } catch (error) {
+      expect(error.status).toBe(404);
+      expect(error.message).toBe('Address not found');
+    }
+  });
+
+  it('should delete an address by id', async () => {
+    const result = await sut.deleteAddress(address.id);
+
+    expect(result).toBeDefined();
+  });
+
+  it('should return a 404 error when deleting an address that does not exist', async () => {
+    const nonExistentId = 'non-existent-id';
+    try {
+      await sut.deleteAddress(nonExistentId);
+    } catch (error) {
+      expect(error.status).toBe(404);
+      expect(error.message).toBe('Address not found');
+    }
   });
 });
